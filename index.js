c028cef3aef4e3b7e6e389eb9b7ea7cbc3335304
HAN ZAW HTET <hanzawhtet317@gmail.com>
2024-07-19 16:06:48 +0630

    index.js updated

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const cors = require("cors");
 app.use(cors());
+
+app.get("/tasks", async (req, res) => {
+      const data = await db.collection("task").find().toArray();
+      setTimeout(() => {
+            res.json(data);
+      }, 2500); 
+})
+
+app.get("/tasks/:id", async (req, res) => {
+      const id = req.params;
+      const data = await db.collection("task").findOne({_id: new ObjectId(id)});
+
+      return res.json(data);
+})
+
+app.listen(8181, () => {
+      console.log("API server running at port 8181");
+});
