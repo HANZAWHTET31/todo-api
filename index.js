a6b573ed392dc20d7c102b0f79dbd11ec36dc297
HAN ZAW HTET <hanzawhtet317@gmail.com>
2024-07-20 16:10:39 +0630

    updated 1

--- index.js
+++ index.js
@@ -16,14 +16,53 @@ app.get("/tasks", async (req, res) => {
       const data = await db.collection("task").find().toArray();
       setTimeout(() => {
             res.json(data);
-      }, 2500); 
+      }, 2000); 
 })
 
 app.get("/tasks/:id", async (req, res) => {
-      const id = req.params;
+      const {id} = req.params;
       const data = await db.collection("task").findOne({_id: new ObjectId(id)});
 
       return res.json(data);
+})
+
+app.delete("/tasks/:id/delete", async (req, res) => {
+      const {id} = req.params;
+      const result = await db.collection("task").deleteOne({_id: new ObjectId(id)});
+      return res.json(result);
+});
+
+app.put("/tasks/:id/toggle", async (req, res) => {
+      const {id} = req.params;
+      const data = await db.collection("task").findOne({_id: new ObjectId(id)});
+      const result = await db.collection("task").updateOne({_id: new ObjectId(id)}, {$set: {done: !data.done}});
+      return res.json(result);
+})
+
+app.post("/tasks", async (req, res) => {
+      const { name } = req.body;
+
+      if(!name){
+            return res.status(400).json({msg: "Name Required"});
+      }
+
+      const result = await db.collection("task").insertOne({name, done: false});
+
+      return res.json({_id: result.insertedId, name, done: false});
+})
+
+app.put("/tasks/:id", async (req, res) => {
+      const {id} = req.params;
+      const {name} = req.body;
+
+      if(!name) res.status(400).json({msg: "Name Required"});
+
+      try {
+            const result = await db.collection("task").updateOne({_id: new ObjectId(id)}, {$set: {name}});
+            return res.json(result);
+      } catch (error) {
+            return res.sendStatus(500);
+      }
 })
 
 app.listen(8181, () => {
